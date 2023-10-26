6a1e1c06fa60585b2ab2c39a239b5cc97e2658ce
Cecilia <127440950+MCcamposs@users.noreply.github.com>
2023-10-26 13:31:36 +0200

    avances feedback

---
+++ BTS/src/app/data/model/Feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+    constructor(
+        public emeailAddres: string,
+        public PhoneNumber: string,
+        public rate: number,
+        public feedbackTtitle: string,
+        public feedback: string,
+    ){}
+        
+    
+}
